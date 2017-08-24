450316cce522065aca6da6924dd4867ba0dfb91a
jhu4 <jhu4@wpi.edu>
2017-08-24 19:31:32 -0400

    added 3 problem solutions

--- src/ClassEight.java
+++ src/ClassEight.java
@@ -189,13 +189,68 @@ public class ClassEight {
 			end--;
 		}
 	}
+	//----------------------------------------------------------------------------------------
+	List<Integer> allAnagrams(String s, String l) {
+		List<Integer> list = new LinkedList<>();
+		if (s.length() > l.length()) {
+			return list;
+		}
 
+		Map<Character, Integer> map = new HashMap<>();
+		//record how many distinct characters have been matched
+		//if match == map.size() we find an anagram
+		int match = 0;
+
+		//the map records the distinct characters in s and how many characters are needed
+		for (char c : s.toCharArray()) {
+			Integer temp = map.get(c);
+			if (temp == null) {
+				map.put(c, 1);
+			} else {
+				map.put(c, temp + 1);
+			}
+		}
+
+		//we have a sliding window of the size s.length()
+		//we move the sliding window each time and do
+		//1. remove the leftmost character in the previous window
+		//2. add the rightmost character in the current window
+		for (int i = 0; i < l.length(); i++) {
+			char c = l.charAt(i);
+			Integer val = map.get(c);
+
+			//we add the new character
+			if (val != null) {
+				map.put(c, val - 1);
+				if (val - 1 == 0) {
+					match++;
+				}
+			}
+
+			//delete the character
+			if (i >= s.length()) {
+				c = l.charAt(i - s.length());
+				val = map.get(c);
+				if (val != null) {
+					map.put(c, val + 1);
+					if (val == 0) {
+						match --;
+					}
+				}
+			}
+
+			if (map.size() == match) {
+				list.add(i + 1 - s.length());
+			}
+		}
+		return list;
+	}
 
 	//----------------------------------------------------------------------------------------
 	public static void main(String[] args) {
 		ClassEight c8 = new ClassEight();
 		HashSet<Integer> hm = new HashSet<>();
 
-		Util.print(c8.longest("a"));
+		Util.print(c8.allAnagrams("ab", "abcbac"));
 	}
 }
--- src/ClassTen.java
+++ src/ClassTen.java
@@ -1,2 +1,36 @@
 public class ClassTen {
+  public boolean isPowerOfTwo(int number) {
+    if (number <= 0) {
+      return false;
+    }
+
+    int count = 0;
+    while (number > 0) {
+      count += number & 1;
+      number >>= 1;
+    }
+    return count == 1;
+  }
+
+  /*
+  public boolean isPowerOfTwo(int number) {
+    //if assume that the input is a positive number
+    return number != 0 && (number & (number - 1)) == 0;
+  }
+  */
+
+  //---------------------------------------------------------------------------
+  public int diffBits(int a, int b) {
+    int count = 0;
+    for (int c = a ^ b; c != 0; c >>>= 1) { //>>> is a logical right shift, writing 0s into the leftmost bit
+      count += c & 1;
+    }
+
+    return count;
+  }
+
+  public static void main(String[] args) {
+    ClassTen c10 = new ClassTen();
+    Util.print(c10.diffBits(2147483647,-2147483648));
+  }
 }
